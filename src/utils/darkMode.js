0053e59608cdc2b782626ed14cd901b107bbd5a4
Devnielote <danielfc_26@hotmail.com>
2025-04-25 21:59:34 -0600

    Fix body background color when dark mode is active

--- src/utils/darkMode.js
+++ src/utils/darkMode.js
@@ -7,6 +7,7 @@ export function toggleDarkMode(btn, container) {
   toggleBtn.addEventListener("click", () => {
     mainContainer.classList.toggle("dark_mode")
     const isDark = mainContainer.classList.contains("dark_mode");
+    document.body.style.backgroundColor = isDark ? "hsl(207,26%,17%)": "white"
     icon.setAttribute("name", isDark ? "moon": "moon-outline");
   });
 }
