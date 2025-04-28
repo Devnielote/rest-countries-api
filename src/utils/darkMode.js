21101bd4fa7109629ad7f43d9563b56f8d17056b
Devnielote <danielfc_26@hotmail.com>
2025-04-27 21:06:46 -0600

    chore: console.log cleanup

--- src/utils/darkMode.js
+++ src/utils/darkMode.js
@@ -2,7 +2,6 @@ export function toggleDarkMode(btn, container) {
   const toggleBtn = document.querySelector(btn);
   const mainContainer = document.querySelector(container);
   const icon = toggleBtn.querySelector("ion-icon"); 
-  console.log(icon);
 
   toggleBtn.addEventListener("click", () => {
     mainContainer.classList.toggle("dark_mode")
