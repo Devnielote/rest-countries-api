a507a15a547fdef704c374d94b26e7870f28c62d
Devnielote <danielfc_26@hotmail.com>
2025-04-29 17:30:06 -0600

    refactor: renderCountries calls renderCards for each country data returns

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,46 +1,26 @@
-export function renderCards(data) {
-  
+import { createCard } from "./createCard.js";
+
+export function renderCountries(data) {
   const container = document.getElementById("cards-container");
   container.innerHTML = "";
 
-  data.forEach((country) => {
-    const card = document.createElement("div");
-    card.classList.add("country_card_container");
-
-    //Card structure
-    const cardImg = document.createElement("img");
-    const cardInfo = document.createElement("section");
-    cardInfo.classList.add("card_info")
-    const cardTitle = document.createElement("h2");
-    const cardPopulation = document.createElement("p");
-    const populationSpan = document.createElement("span")
-    const cardRegion = document.createElement("p");
-    const regionSpan = document.createElement("span")
-    const cardCapital = document.createElement("p");
-    const capitalSpan = document.createElement("span")
-
-    //Card content
-    
-    cardImg.src = country.flags.png;
-    cardTitle.innerText = country.name.common;
-    cardPopulation.innerText = "Population: "
-    populationSpan.innerText = country.population.toLocaleString(); 
-    cardPopulation.append(populationSpan);
-    cardRegion.innerText = "Region: " 
-    regionSpan.innerText = country.region; 
-    cardRegion.append(regionSpan);
-    cardCapital.innerText = "Capital: ";
-    capitalSpan.innerText = country.capital;
-    cardCapital.append(capitalSpan);
-
-    //Card render 
-    cardInfo.append(cardTitle, cardPopulation, cardRegion, cardCapital);
-    card.append(cardImg, cardInfo);
-    container.append(card);
-  });
+  try {
+    if (Array.isArray(data)){
+      data.forEach(country => {
+        const card = createCard(country)
+        container.append(card);
+      });
+      
+    } else {
+      const card = createCard(data);
+      container.append(card);
+    }
+  } catch(e) {
+    console.error("Error rendering country cards", e)
+  }
 }
 
-export function renderCard(data) {
+export function renderCountryDetails(data) {
 
   const {
     name:{common, nativeName},
