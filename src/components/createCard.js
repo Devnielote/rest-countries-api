ad37eea6db8fb29de368c37840a32f341ccd180a
Devnielote <danielfc_26@hotmail.com>
2025-04-29 17:14:56 -0600

    Create function to handle country cards generation

---
+++ src/components/createCard.js
@@ -0,0 +1,37 @@
+export function createCard(country){
+
+  const card = document.createElement("div");
+  card.classList.add("country_card_container");
+
+  //Card structure
+  const cardImg = document.createElement("img");
+  const cardInfo = document.createElement("section");
+  cardInfo.classList.add("card_info")
+  const cardTitle = document.createElement("h2");
+  const cardPopulation = document.createElement("p");
+  const populationSpan = document.createElement("span")
+  const cardRegion = document.createElement("p");
+  const regionSpan = document.createElement("span")
+  const cardCapital = document.createElement("p");
+  const capitalSpan = document.createElement("span")
+
+  //Card content
+
+  cardImg.src = country.flags.png;
+  cardTitle.innerText = country.name.common;
+  cardPopulation.innerText = "Population: "
+  populationSpan.innerText = country.population.toLocaleString(); 
+  cardPopulation.append(populationSpan);
+  cardRegion.innerText = "Region: " 
+  regionSpan.innerText = country.region; 
+  cardRegion.append(regionSpan);
+  cardCapital.innerText = "Capital: ";
+  capitalSpan.innerText = country.capital;
+  cardCapital.append(capitalSpan);
+
+  //Card render 
+  cardInfo.append(cardTitle, cardPopulation, cardRegion, cardCapital);
+  card.append(cardImg, cardInfo);
+
+  return card;
+}
