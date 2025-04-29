f1d83718658e203710370b6491aa87865bf38afc
Devnielote <danielfc_26@hotmail.com>
2025-04-29 17:20:51 -0600

    Import renamed function + import wip go:q

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 import { fetchAllCountries } from "./src/api.js";
-import { renderCards } from "./src/components/cards.js";
+import { renderCountries } from "./src/components/cards.js";
 import { toggleDarkMode } from "./src/utils/darkMode.js";
+import { handleCountrySearch } from "./src/utils/searchByName.js";
 import { handleRegionSelect } from "./src/utils/selectByRegion.js";
+import { goBackPage } from "./src/utils/backButton.js";
 
 async function main() {
   try {
     toggleDarkMode("#dark-mode-toggle", "#main-container")
+    goBackPage("#back-btn");
     const data = await fetchAllCountries();
-    renderCards(data);
+    renderCountries(data);
     handleRegionSelect("regions-select");
+    handleCountrySearch("search-input");
   } catch(error) {
     console.error("Mistakes were made in main:", error);
   }
